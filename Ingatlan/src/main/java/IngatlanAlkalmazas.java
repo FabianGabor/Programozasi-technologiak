3681e2fcd3106fcec3240acc00daf6f4e301a99e
Fabian Gabor <info@fabiangabor.com>
2020-10-13 12:47:06 +0200

    IngatlanAlkalmazas class init

---
+++ Ingatlan/src/main/java/IngatlanAlkalmazas.java
@@ -0,0 +1,34 @@
+public class IngatlanAlkalmazas implements IngatlanAlkalmazasInterface{
+    @Override
+    public double nm_osszeg() {
+        return 0;
+    }
+
+    @Override
+    public int ar_osszeg() {
+        return 0;
+    }
+
+    @Override
+    public double minimum_ar() {
+        return 0;
+    }
+
+    @Override
+    public Ingatlan legdragabb_budapesti_lakas() {
+        return null;
+    }
+
+    @Override
+    public int osszesitett_ar() {
+        return 0;
+    }
+
+    @Override
+    public int teljes_arak_osszege() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+    }
+}
