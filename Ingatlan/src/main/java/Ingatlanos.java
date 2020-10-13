b58adb6fe51f3918bdcd60fc3ef2e39c754cc833
Fabian Gabor <info@fabiangabor.com>
2020-10-13 12:37:54 +0200

    Ingatlanos

---
+++ Ingatlan/src/main/java/Ingatlanos.java
@@ -0,0 +1,13 @@
+import java.util.TreeSet;
+
+public class Ingatlanos {
+    static TreeSet <Ingatlan> ingatlanok;
+    static int panellakas;
+
+    void feltolt() {
+        /**
+         * ingatlanok.txt
+         * osztály megnevezése#telepules#ar#nm#szobaszam#tipus#emelet#szigetelt(igen/nem) (ez utóbbi kettő csak akkor, ha az osztály Panel)
+         */
+    }
+}
