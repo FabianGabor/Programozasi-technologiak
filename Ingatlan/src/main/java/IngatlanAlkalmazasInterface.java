4a124acb85ad98e8127b91d13a6d62e49938a1c5
Fabian Gabor <info@fabiangabor.com>
2020-10-13 12:45:58 +0200

    IngatlanAlkalmazasInterface

---
+++ Ingatlan/src/main/java/IngatlanAlkalmazasInterface.java
@@ -0,0 +1,8 @@
+public interface IngatlanAlkalmazasInterface {
+    double nm_osszeg();
+    int ar_osszeg();
+    double minimum_ar();
+    Ingatlan legdragabb_budapesti_lakas();
+    int osszesitett_ar();
+    int teljes_arak_osszege();
+}
