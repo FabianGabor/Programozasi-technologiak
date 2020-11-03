1929ee5ba7376891be1919810bdac9aa95fa8a76
FabianGabor <info@fabiangabor.com>
2020-11-03 17:18:32 +0100

    JavaFX

---
+++ Szamologep/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module Szamologep.main {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+
+    opens org.openjfx to javafx.fxml;
+    exports org.openjfx;
+}
---
+++ Szamologep/src/main/java/org/openjfx/SzamologepGUI.java
@@ -0,0 +1,8 @@
+package org.openjfx;
+
+import javax.swing.*;
+
+public class SzamologepGUI {
+    private JButton button1;
+    private JPanel panel1;
+}
